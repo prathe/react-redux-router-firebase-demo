3648f939f29bfa0b50d43e268ab4d683589379ab
Philippe Rathé <prathe@gmail.com>
2018-10-30 18:27:28 -0400

    apply routes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import { Link } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
+import Routes from "./Routes";
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
+      <div className="container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <Link to="/">Scratch</Link>
+              <Link to="/">Home</Link>
             </Navbar.Brand>
             <Navbar.Toggle />
           </Navbar.Header>
         </Navbar>
+        <Routes />
       </div>
     );
   }
