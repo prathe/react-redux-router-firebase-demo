b798fbc51369ba95cdcc5df66b52077ddc2b3774
Philippe Rathé <prathe@gmail.com>
2018-10-30 20:37:12 -0400

    Use SPA links.

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./resources/Home";
+import NotFound from "./resources/NotFound";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
+
+    // This is a "soft not found page" which should
+    // never be rendered when using links within the apps of a SPA.
+    <Route component={NotFound}/>
   </Switch>;
---
+++ src/resources/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Our soft Not Found page, which should never be rendered unless we mess up.
+export default () => {
+  return (
+    <h1>Not Found.</h1>
+  );
+};
