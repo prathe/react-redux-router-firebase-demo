276523ce187891698ff79b01342baba1369bae5e
Philippe Rathé <prathe@gmail.com>
2018-11-01 17:00:38 -0400

    Add login and sign up views

--- src/views/RoutedViews.js
+++ src/views/RoutedViews.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
 import NotFound from "./NotFound";
 
 const RoutedViews = () =>
   <Switch>
     <Route path="/" exact component={Home} />
+    <Route path="/login" exact component={Login} />
+    <Route path="/signup" exact component={SignUp} />
 
     { /* This is a "soft not found page" which should
          never be rendered when using links within the apps of a SPA. */ }
